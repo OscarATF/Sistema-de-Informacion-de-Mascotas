f4fa365bdce5d517271fb2cf01179501caaf6bb0
Yair292 <dluquep@unjbg.edu.pe>
2024-07-17 12:20:37 -0500

    Agregar estructura para opciones internas

---
+++ estructuras.h
@@ -0,0 +1,14 @@
+#ifndef ESTRUCTURAS_H
+#define ESTRUCTURAS_H
+
+#include <string>
+
+//Definición del struct Propietario.
+struct Propietario {
+    int id;
+    string nombre;
+    string contacto;
+    int mascota_id;
+};
+
+#endif //ESTRUCTURAS_H
--- menu.cpp
+++ menu.cpp
@@ -1,24 +1,58 @@
 #include <iostream>
 #include "menu.h"
 using namespace std;
-
-void Menu() {
-	int op;
+int op;
+//funcion menu principal
+void Menu() {	
 	do {
 		//se limpia la consola
 		system("cls");
-		//menu principal
-		cout<<"Sistema de gestión veterinaria\n\n";
-		cout<<"1. Registrar Propietario"<<endl
+		cout<<"\nSistema de gestión veterinaria\n\n";
+		cout<<"1. Propietario"<<endl
 			<<"2. Registrar Mascota"<<endl
 			<<"3. Eliminar Mascota"<<endl
-			<<"4. Eliminar Propietario"<<endl
 			<<"5. Actualizar Informacion de Mascotas"<<endl
-			<<"6. Actualizar Informacion de Propietario"<<endl
 			<<"7. Buscar Mascota por ID"<<endl
-			<<"8. Buscar Propietario por ID"<<endl
 			<<"9. Salir del programa"<<endl;
 		cout<<"\nDigite una opcion: ";
 		cin>>op;
+		switch (op){
+			case 1:
+				menuPropietario();
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+		}
 	} while (op!=9);
+}
+//funcion mascota
+
+void menuPropietario(){
+	system("cls");
+	cout<<"\n Opciones del Propietario\n\n";
+	cout<<"1. Registrar Propietario\n"
+		<<"2. Actualizar Datos del Propietario\n"
+		<<"3. Buscar Propietario por ID\n"
+		<<"4. Eliminar Propietario\n"
+		<<"5. Volver al menu principal\n";
+		cout<<"\nDigite una opcion: ";
+	cin>>op;
+	switch (op){
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			Menu();
+			break;
+		default:
+			Menu();
+	
+	}
 }
