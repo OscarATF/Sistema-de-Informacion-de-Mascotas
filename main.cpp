940b56abe8d09e187295d9bfdbf45e05f155af51
Oscar <oticonaf@unjbg.edu.pe>
2024-07-17 10:59:46 -0500

    Colocar Menu como archivo de cabecera

--- main.cpp
+++ main.cpp
@@ -1,24 +1,7 @@
 #include <iostream>
 #include <windows.h>
+#include "menu.h"
 using namespace std;
-
-void Menu() {
-int op;
-	do {
-		cout<<"Sistema de gestión veterinaria\n"<<endl;
-		cout<<"1. Registrar Propietario"<<endl
-			<<"2. Registrar Mascota"<<endl
-			<<"3. Eliminar Mascota"<<endl
-			<<"4. Eliminar Propietario"<<endl
-			<<"5. Actualizar Informacion de Mascotas"<<endl
-			<<"6. Actualizar Informacion de Propietario"<<endl
-			<<"7. Buscar Mascota por ID"<<endl
-			<<"8. Buscar Propietario por ID"<<endl
-			<<"9. Salir del programa"<<endl;
-		cout<<"\nIngrese una opcion: ";
-		cin>>op;
-	} while (op!=9);
-}
 
 int main() {
 	SetConsoleOutputCP(CP_UTF8);
