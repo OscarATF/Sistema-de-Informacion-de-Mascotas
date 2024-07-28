29536da2e7c564cedbc9daef080670cc7195f8f7
Yair292 <dluquep@unjbg.edu.pe>
2024-07-28 11:27:58 -0500

    Validar entrada en fechas de ventas.

--- registroDeVentas.cpp
+++ registroDeVentas.cpp
@@ -4,41 +4,54 @@
 #include "variablesGlobales.h"
 #include "inventario.h" // esto es para listar los productos
 
-void registrarVentas() {	
+void registrarVentas() {
+	int cantidadCaracteres;	
 	system("cls");		
     if (cantidaddeventas >= capacidadMaxima) {
         cout << "No se pueden agregar más ventas, límite alcanzado.\n";
         return;
-    } else {
-    	int seleccion;
-	    Ventas p; //para la id
-	    p.id = cantidaddeventas + 1;
-	    cout << "Ingrese nombre del cliente: ";
-	    cin.ignore();
-	    getline(cin, p.nombreDelCliente);
-	    cout << "Ingrese la fecha: ";
+	} 
+	int seleccion;
+	Ventas p; 
+	cout << "Ingrese nombre del cliente: ";
+	cin.ignore();
+	getline(cin, p.nombreDelCliente);
+	do{ //Este bucle es Validacion, permitira volver a preguntar la fecha al usuario.
+		cout << "Ingrese la fecha (DD/MM/YYYY): ";
 	    getline(cin, p.fecha);
-	    cout << "Ingrese el metodo de pago: ";
-	    getline(cin, p.metodoDePago);
-	    mostrarInventario();
-	    cout << "\nIngrese el numero del producto: "; cin >> p.idproductoComprado;
-	    p.datosdelproducto=productos[p.idproductoComprado-1];
-	    if(productos[p.idproductoComprado-1].cantidad == 0){	
-    		cout << "\n| Lo lamento, producto no disponible |\n\n";
-		}else{	
-	    	cout << "Ingrese la cantidad de venta: "; cin >> p.cantidad;
-	    	if(p.cantidad <= productos[p.idproductoComprado-1].cantidad){ 
-		    	p.precioDeVenta = p.cantidad*p.datosdelproducto.precio;
-		    	productos[p.idproductoComprado-1].cantidad-=p.cantidad;
-		    	ventas[cantidaddeventas] = p;
-		    	cantidaddeventas++;
-		    	cout << "\nLa venta a sido registrada correctamente.\n\n";
-			}else{
-				cout<<"\n ERROR - CANTIDAD EXCESIVA\n\n";
-			}
-	 	}
+		cantidadCaracteres = p.fecha.length();
+		
+		//Esto valida
+		if (p.fecha[2]!= '/' || p.fecha[5]!= '/'){ 
+			cantidadCaracteres = 9; // se da 9 para volver al bucle
+		}
+		if(cantidadCaracteres != 10){  //Esto es para el error en el caso de que sea diferente de 10.
+			cout << "\nError fecha invalida, INGRESE DE NUEVO.\n\n";
+		}
+	}while(cantidadCaracteres != 10);
+
+
+
+	cout << "Ingrese el metodo de pago: ";
+	getline(cin, p.metodoDePago);
+	mostrarInventario();
+	cout << "\nIngrese el numero del producto: "; cin >> p.idproductoComprado;
+	p.datosdelproducto=productos[p.idproductoComprado-1];
+	if(productos[p.idproductoComprado-1].cantidad == 0){	
+	    cout << "\n| Lo lamento, producto no disponible |\n\n";
+	}else{	
+		cout << "Ingrese la cantidad de venta: "; cin >> p.cantidad;
+		if(p.cantidad <= productos[p.idproductoComprado-1].cantidad){ 
+			p.precioDeVenta = p.cantidad*p.datosdelproducto.precio;
+			productos[p.idproductoComprado-1].cantidad-=p.cantidad;
+			ventas[cantidaddeventas] = p;
+			cantidaddeventas++;
+			cout << "\nLa venta a sido registrada correctamente.\n\n";
+		}else{
+			cout<<"\n ERROR - CANTIDAD EXCESIVA\n\n";
+		}
+	}    		
 	system("pause");
-	}
 }
 
 void mostrarVentas(){
@@ -77,3 +90,4 @@ void buscarVentas(){
 	}
 	system("pause");
 }
+
